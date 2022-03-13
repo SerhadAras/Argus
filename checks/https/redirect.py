51b843b422a8963fe2f13165f412a8422c1b292e
arash-nouri-1 <76052712+arash-nouri-1@users.noreply.github.com>
2022-03-13 13:09:31 +0100

    Redirect https check (#87)

---
+++ checks/https/redirect.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+from urllib import request
+import requests
+import sys
+
+import urllib3
+
+DOMAIN = sys.argv[1]
+url = 'http://' + DOMAIN
+HTTPS = "https"
+urllib3.disable_warnings()
+
+try:
+    res = requests.get(url, verify=False)
+except:
+    print("Dit domein bestaat niet.", file=sys.stderr)
+
+else:
+    new_url = res.url
+    if HTTPS in new_url:
+        print(f'{{"name": "redirect", "score": 10, "message": Alle potentieel onveilige verkeer via {url}, wordt veilig doorgestuurd naar {new_url}"}}')
+    else:
+        print(f'{{"name": "redirect", "score": 0, "message": "Uw website {url} is bereikbaar via het onbeveiligde http, dit betekent dat in bepaalde gevallen bezoekers onversleuteld gegevens naar uw website kunnen doorsturen. Misschien kan u een redirectie naar https opzetten?"}}')
