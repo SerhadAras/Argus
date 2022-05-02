08560e02b834d7e3f10c56f42a8fc17f955d1c25
Mathias Maes <watcherwhale@maes.family>
2022-05-02 08:57:12 +0200

    Deploy Metrics Server (#168)

--- services/.actions/build.sh
+++ services/.actions/build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+services=( "gateway" "sequencer" "metrics" )
+
 if [ -z "$1" ];
 then
     REGISTRY="ghcr.io/watcherwhale"
@@ -9,6 +11,9 @@ else
     REGISTRY="$1"
 fi
 
-docker build --no-cache -f Dockerfile --build-arg service=gateway -t $REGISTRY/gateway .
-docker build --no-cache -f Dockerfile --build-arg service=sequencer -t $REGISTRY/sequencer .
+for service in ${services[@]}
+do
+    docker build --no-cache -f Dockerfile --build-arg service=$service -t $REGISTRY/$service .
+done
+
 docker build --no-cache -f Dockerfile.cert --build-arg service=cert-master -t $REGISTRY/cert-master .
--- services/.actions/push.sh
+++ services/.actions/push.sh
@@ -2,27 +2,22 @@
 
 set -e
 
-if [ "$1" ];
-then
-    REGISTRY="$1"
+services=( "gateway" "sequencer" "cert-master" "metrics" )
 
-    docker tag $REGISTRY/gateway:latest ghcr.io/watcherwhale/gateway:latest
-    docker tag $REGISTRY/sequencer:latest ghcr.io/watcherwhale/sequencer:latest
-    docker tag $REGISTRY/cert-master:latest ghcr.io/watcherwhale/cert-master:latest
-fi
+for service in ${services[@]}
+do
 
-GATEWAY_VERSION="$(jq -r .version sequencer/package.json)"
-SEQUENCER_VERSION="$(jq -r .version sequencer/package.json)"
-CERT_MASTER_VERSION="$(jq -r .version sequencer/package.json)"
+    if [ "$1" ];
+    then
+        REGISTRY="$1"
+        docker tag $REGISTRY/$service:latest ghcr.io/watcherwhale/$service:latest
+    fi
 
-docker tag ghcr.io/watcherwhale/gateway:latest ghcr.io/watcherwhale/gateway:v$GATEWAY_VERSION
-docker tag ghcr.io/watcherwhale/sequencer:latest ghcr.io/watcherwhale/sequencer:v$SEQUENCER_VERSION
-docker tag ghcr.io/watcherwhale/cert-master:latest ghcr.io/watcherwhale/cert-master:v$CERT_MASTER_VERSION
+    VERSION="$(jq -r .version sequencer/package.json)"
 
-docker push ghcr.io/watcherwhale/gateway:latest
-docker push ghcr.io/watcherwhale/sequencer:latest
-docker push ghcr.io/watcherwhale/cert-master:latest
+    docker tag ghcr.io/watcherwhale/$service:latest ghcr.io/watcherwhale/$service:v$VERSION
 
-docker push ghcr.io/watcherwhale/gateway:v$GATEWAY_VERSION
-docker push ghcr.io/watcherwhale/sequencer:v$SEQUENCER_VERSION
-docker push ghcr.io/watcherwhale/cert-master:v$CERT_MASTER_VERSION
+    docker push ghcr.io/watcherwhale/$service:latest
+    docker push ghcr.io/watcherwhale/$service:v$VERSION
+
+done
